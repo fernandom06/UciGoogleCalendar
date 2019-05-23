062b50d3a1dac7ba95131e59b75b9b93a1713900
Fernando <fernandom06@hotmail.com>
2019-05-23 22:33:00 +0200

    Extract data in arrays

--- read_excel.py
+++ read_excel.py
@@ -15,14 +15,33 @@ class OpenData:
 
     """Extrae los datos obviando las cabeceras"""
     def __extract_data(self):
-        pass
+        self.date_from = []
+        self.date_to = []
+        self.name = []
+        self.venue = []
+        self.country = []
+        self.category = []
+        self.calendar = []
+        self.uci_class = []
+        self.email = []
+        self.web = []
+        for row in self.raw_data._cell_values:
+            self.date_from.append(row[0])
+            self.date_to.append(row[1])
+            self.name.append(row[2])
+            self.venue.append(row[3])
+            self.country.append(row[4])
+            self.category.append(row[5])
+            self.calendar.append(row[6])
+            self.uci_class.append(row[7])
+            self.email.append(row[8])
+            self.web.append(row[9])
+
+
 
     def __delete_headers(self):
-        print(self.raw_data.nrows)
         for index in range(0, self.count_headers_rows):
             self.raw_data._cell_values.pop(0)
-        self.raw_data.nrows = len(self.raw_data._cell_values)
-        print(self.raw_data.nrows)
         return self.raw_data
 
 
