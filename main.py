7cf8d711ef4d8d61d94a9ebee0db1f178abc600c
Fernando <fernandom06@hotmail.com>
2019-05-23 21:45:40 +0200

    Delete headers

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from read_excel import OpenData
 
 FILENAME = 'data/Calendar_ROA_5_20_2019.xlsx'
 
-raw_data = OpenData(filename=FILENAME).process_data()
+raw_data = OpenData(filename=FILENAME, count_headers_rows=2).process_data()
 print('minin')
--- read_excel.py
+++ read_excel.py
@@ -1,17 +1,28 @@
 from xlrd import open_workbook
 
 class OpenData:
-    def __init__(self, filename):
+    def __init__(self, filename, count_headers_rows):
         self.filename = filename
+        self.count_headers_rows = count_headers_rows
+        self.raw_data = self.open_excel()
 
     def open_excel(self):
         return open_workbook(filename=self.filename).sheet_by_index(0)
 
     def process_data(self):
-        raw_data = self.open_excel()
+        self.__delete_headers()
         self.__extract_data()
 
     """Extrae los datos obviando las cabeceras"""
     def __extract_data(self):
         pass
+
+    def __delete_headers(self):
+        print(self.raw_data.nrows)
+        for index in range(0, self.count_headers_rows):
+            self.raw_data._cell_values.pop(0)
+        self.raw_data.nrows = len(self.raw_data._cell_values)
+        print(self.raw_data.nrows)
+        return self.raw_data
+
 
